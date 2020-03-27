a20e8e0451c6006d9f53c3c9d4d5e9212b6bb732
majian <46617237@qq.com>
2020-03-27 11:31:42 +0800

    添加项目结构

---
+++ java/springboot-mybatis/scaffold-api/src/main/java/com/scaffold/springboot/mybatis/api/ScaffoldApiApplication.java
@@ -0,0 +1,13 @@
+package com.scaffold.springboot.mybatis.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaffoldApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScaffoldApiApplication.class, args);
+    }
+
+}
---
+++ java/springboot-mybatis/scaffold-api/src/test/java/com/scaffold/springboot/mybatis/api/ScaffoldApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaffold.springboot.mybatis.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScaffoldApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
